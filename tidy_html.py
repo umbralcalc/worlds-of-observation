4f419cf06de3ddb760879cb55fce61468c4684cb
umbralcalc <umbralcalc@gmail.com>
2023-02-27 08:10:20 +0000

    added some more text on poisson processes

--- tidy_html.py
+++ tidy_html.py
@@ -5,6 +5,7 @@ def main():
     with open("index.html", "r") as f:
         soup = bs(f.read(), "html.parser")
         i = 1
+        eq_references = {}
         for span in soup.find_all("span"):
             attr = span.attrs.get("class")
             if attr is None:
@@ -24,15 +25,38 @@ def main():
                             current_tag_text = tag.text
                             if current_tag_text[1:-1] != tag_text:
                                 continue
+                            eq_references[tag_text] = i
                             if current_tag_text[0] == "[" and current_tag_text[-1] == "]":
                                 replace_tag_text = "[" + tag_text + "]"
                             tag.replace_with(str(i).join(tag.split(replace_tag_text)))
                         j += 1
                 i += 1
+        for link in soup.find_all("a"):
+            if link.attrs.get("data-reference-type") is None:
+                continue
+            if link.attrs.get("data-reference") is None:
+                continue
+            eq_num = eq_references.get(link.attrs.get("data-reference"))
+            if eq_num is None:
+                continue
+            link.attrs["href"] = "#" + str(eq_num)
+            link.attrs["data-reference"] = eq_num
     
     html = soup.prettify("utf-8")
     with open("index.html", "wb") as f:
         f.write(html)
+
+    # read file
+    with open("index.html", 'r') as fp:
+        # read an store all lines into list
+        lines = fp.readlines()
+    
+    # Write file
+    with open("index.html", 'w') as fp:
+        # iterate each line
+        for number, line in enumerate(lines):
+            if (number < 122) or (number > 140):
+                fp.write(line)
 
 if __name__ == "__main__":
     main()
